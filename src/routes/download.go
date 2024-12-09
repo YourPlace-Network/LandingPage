25fd7eb19e0c63b3d491d8c60c66d08774948a96
Nops <fake@email.com>
2024-12-09 18:24:02 -0500

    working download page

--- src/routes/download.go
+++ src/routes/download.go
@@ -1,18 +1,41 @@
 package routes
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/csrf"
+	"html/template"
 	"net/http"
 )
 
 func DownloadRoutes(router *gin.Engine, title string) {
+	currentVersion := "0.1.0"
+	bucket := "https://yourplace-downloads.nyc3.digitaloceanspaces.com"
+	downloads := map[string]map[string]string{
+		"version": {
+			"latest": currentVersion,
+		},
+		"windows": {
+			"amd64": bucket + "/win-amd64/" + currentVersion + "/YourPlace.exe",
+		},
+		"osx": {
+			"amd64": bucket + "/osx-amd64/" + currentVersion + "/YourPlace",
+			"arm64": bucket + "/osx-arm64/" + currentVersion + "/YourPlace",
+		},
+	}
+	downloadJson, _ := json.Marshal(downloads)
+
 	router.GET("/download", func(c *gin.Context) {
 		token := csrf.Token(c.Request)
 		c.HTML(http.StatusOK, "src/templates/pages/download.tmpl", gin.H{
 			"title":     title,
 			"pageName":  "download",
 			"csrfToken": token,
+			"downloads": template.JS(downloadJson), // use template.JS to safely inject JavaScript
 		})
+	})
+
+	router.GET("/version", func(c *gin.Context) {
+		c.SecureJSON(http.StatusOK, gin.H{"version": currentVersion})
 	})
 }
--- src/typescript/pages/download.ts
+++ src/typescript/pages/download.ts
@@ -7,9 +7,129 @@ declare global { // Extend the window interface with public objects
         LoginCallback: () => void;
         PageReloadCallback: () => void;
         DisconnectWalletCallback: () => void;
+        downloadJson: DownloadLinks;
     }
 }
 
-window.addEventListener("load", () => {
+interface ClientInfo {
+    os: string;
+    architecture: string;
+    userAgent: string;
+}
+interface DownloadLinks {
+    [key: string]: {
+        [key: string]: string;
+    }
+}
 
+
+
+window.addEventListener("load", () => {
+    let DOM = {
+        recommendedLink: document.getElementById("recommendedLink") as HTMLAnchorElement,
+        recommendedText: document.getElementById("recommendedText") as HTMLParagraphElement,
+        recommendedImg: document.getElementById("recommendedImg") as HTMLImageElement,
+        osxArch: document.getElementById("osxArch") as HTMLSelectElement,
+        osxDiv: document.getElementById("osxDiv") as HTMLDivElement,
+        winArch: document.getElementById("winArch") as HTMLSelectElement,
+        winDiv: document.getElementById("winDiv") as HTMLDivElement,
+    }
+    const downloadLinks: DownloadLinks = window.downloadJson;
+
+    async function main() {
+        setRecommendedDownload();
+    }
+    function setRecommendedDownload() {
+        const clientInfo = detectClientInfo();
+        if (clientInfo.os in downloadLinks) {
+            if (clientInfo.architecture in downloadLinks[clientInfo.os]) {
+                DOM.recommendedLink.href = downloadLinks[clientInfo.os][clientInfo.architecture];
+                if (clientInfo.os === "osx") {
+                    DOM.recommendedText.textContent = "MacOS";
+                    DOM.recommendedImg.src = `/static/image/apple.svg`;
+                } else if (clientInfo.os === "windows") {
+                    DOM.recommendedText.textContent = "Microsoft Windows"
+                    DOM.recommendedImg.src = `/static/image/windows.svg`;
+                }
+            }
+        }
+    }
+    function detectClientInfo(): ClientInfo {
+        const ua = navigator.userAgent;
+        let os = "unknown";
+        let architecture = "unknown";
+        if (ua.indexOf("Win") != -1) {
+            os = "windows";
+        } else if (ua.indexOf("Mac") != -1) {
+            os = "osx";
+        } else if (ua.indexOf("Linux") != -1) {
+            os = "linux";
+        } else if (ua.indexOf("Android") != -1) {
+            os = "android";
+        } else if (ua.indexOf("iOS") != -1 || ua.indexOf("iPhone") != -1 || ua.indexOf("iPad") != -1) {
+            os = "ios";
+        }
+        if (ua.indexOf("x64") !== -1 || ua.indexOf("WOW64") !== -1 || ua.indexOf("x86_64") !== -1) {
+            architecture = "amd64";
+        } else if (ua.indexOf("x86") !== -1 || ua.indexOf("i686") !== -1) {
+            architecture = "amd86";
+        } else if (ua.indexOf("arm") !== -1 || ua.indexOf("ARM") !== -1) {
+            architecture = "arm64";
+        } else if (ua.indexOf("aarch64") !== -1) {
+            architecture = "arm64";
+        }
+        if (os === "MacOS" && architecture === "Unknown") {
+            if (/Mac OS X/.test(ua)) {
+                architecture = "arm64"; // Modern Macs are likely to be ARM-based
+            }
+        }
+        return {os, architecture, userAgent: ua};
+    }
+    async function downloadOSX() {
+        try {
+            const response = await fetch(downloadLinks["osx"][DOM.osxArch.value]);
+            if (!response.ok) {
+                console.log("Failed to download file");
+                return;
+            }
+            const blob = await response.blob();
+            const downloadURL = window.URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.href = downloadURL;
+            link.download = "YourPlace";
+            link.style.display = "none";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(downloadURL);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    async function downloadWindows() {
+        try {
+            const response = await fetch(downloadLinks["windows"][DOM.winArch.value]);
+            if (!response.ok) {
+                console.log("Failed to download file");
+                return;
+            }
+            const blob = await response.blob();
+            const downloadURL = window.URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.href = downloadURL;
+            link.download = "YourPlace.exe";
+            link.style.display = "none";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(downloadURL);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    DOM.osxDiv.addEventListener("click", downloadOSX);
+    DOM.winDiv.addEventListener("click", downloadWindows);
+
+    main().then();
 });
