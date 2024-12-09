87628714b3cfc20f68866f02d7ff43bdfc2504e0
Nops <fake@email.com>
2024-12-09 05:32:31 -0500

    tls testing

--- main.go
+++ main.go
@@ -45,7 +45,11 @@ func main() {
 	}
 	_, _ = db.Exec("CREATE TABLE IF NOT EXISTS subscribers (email TEXT PRIMARY KEY, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
 
-	gin.SetMode(gin.ReleaseMode)
+	if os.Getenv("GIN_MODE") == "debug" {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	router := gin.Default()
 	LoadTemplates(router, templateFS, "src/templates/*tmpl")
 	router.Use(middleware.ContentTypeMiddleware())
@@ -65,9 +69,23 @@ func main() {
 		csrf.Secure(true), csrf.HttpOnly(true), csrf.Path("/"))
 	var srv *http.Server
 	if ifFileExists("cert.pem") && ifFileExists("cert.key") {
-		tlsConfig, err := loadTLSConfig("cert.pem", "cert.key")
+		/*tlsConfig, err := loadTLSConfig("cert.pem", "cert.key")
 		if err != nil {
 			log.Fatalln("Could not load TLS config: " + err.Error())
+		}*/
+		cert, err := tls.LoadX509KeyPair("cert.pem", "cert.key")
+		if err != nil {
+			log.Fatalf("Failed to load certificate: %v", err)
+		}
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			MinVersion:   tls.VersionTLS12,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+			},
+			PreferServerCipherSuites: true,
 		}
 		srv = &http.Server{
 			Addr:      ":443",
