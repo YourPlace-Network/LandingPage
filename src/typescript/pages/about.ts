12940d5d76203bc2d72a99836467817f7a0bc689
Nops <fake@email.com>
2024-12-09 19:39:45 -0500

    typescript reorg

--- src/typescript/pages/about.ts
+++ src/typescript/pages/about.ts
@@ -1,6 +1,6 @@
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle');
 import "../../scss/pages/about.scss";
-import "../../scss/components/menu.scss";
+import "../components/menu";
 
 declare global { // Extend the window interface with public objects
     interface Window {
